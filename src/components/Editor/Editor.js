a1938ef43955ded0ac3a8d3535c380b0d664d606
Simona Pizio <simona.pizio@gmail.com>
2024-02-27 18:17:19 +0100

    develop - #1 editor - set app and create editor component

---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Editor.css';
+
+export default function Editor () {
+
+  return (
+    <div id='editorContainer'>
+      <textarea id='editor'></textarea>
+    </div>
+  );
+}
