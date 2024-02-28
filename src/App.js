8ddd8b6f973fef979cd91e8b6070b28cde9fb8bd
Simona Pizio <simona.pizio@gmail.com>
2024-02-28 14:49:38 +0100

    develop - #2 previewer - create previewer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
 import Editor from './components/Editor/Editor';
+import Previewer from './components/Previewer/Previewer';
 
 function App() {
   return (
     <div className="App">
       <Editor />
+      <Previewer />
     </div>
   );
 }
---
+++ src/components/Previewer/Previewer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Previewer.css';
+
+export default function Previewer () {
+
+  return (
+    <div id='prevContainer'>
+      <div>
+        <h2>Previewer</h2>
+      </div>
+      <div id='previewer'></div>
+    </div>
+  );
+}
